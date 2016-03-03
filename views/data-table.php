c767d886913582b009e70f0f6a44204edf1caacc
Dan Stevens <stevens@earthit.com>
2016-03-03 12:12:53 -0500

    Delete some unused stuff.

--- views/data-table.php
+++
@@ -1,9 +0,0 @@
-<?php $PU->emitHtmlBoilerplate($collectionName, $params); ?>
-
-<?php if(count($items) === 0): ?>
-<p>No data!</p>
-<?php else: ?>
-<?php EarthIT_PAXML::emit($tablePaxml); ?>
-<?php endif; ?>
-
-<?php $PU->emitHtmlFooter(); ?>
--- views/say-hi-to.php
+++
@@ -1,5 +0,0 @@
-<?php $PU->emitHtmlBoilerplate("Hi!", $params); ?>
-
-<p style="font-size:x-large">Hello, <?php eht($name); ?>!</p>
-
-<?php $PU->emitHtmlFooter(); ?>
